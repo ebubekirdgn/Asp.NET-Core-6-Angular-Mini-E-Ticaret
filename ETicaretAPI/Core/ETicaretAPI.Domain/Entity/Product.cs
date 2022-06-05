d37017725ea5bb0f27ec66b90a222d10fe98e048
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-05 17:06:17 +0300

    update

---
+++ ETicaretAPI/Core/ETicaretAPI.Domain/Entity/Product.cs
@@ -0,0 +1,11 @@
+﻿using ETicaretAPI.Domain.Entity.Common;
+
+namespace ETicaretAPI.Domain.Entity
+{
+    public class Product : BaseEntity
+    {
+        public string? Name { get; set; }
+        public int Stock { get; set; }
+        public long Price{ get; set; }
+    }
+}
