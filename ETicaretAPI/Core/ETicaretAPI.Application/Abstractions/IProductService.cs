9286e22899df924bc189e685ff5658b87a4e356c
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-09 00:58:02 +0300

    Repository added

--- ETicaretAPI/Core/ETicaretAPI.Application/Abstractions/IProductService.cs
+++ ETicaretAPI/Core/ETicaretAPI.Application/Abstractions/IProductService.cs
@@ -1,9 +1,12 @@
-﻿using ETicaretAPI.Domain.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Abstractions
 {
-    public interface IProductService
+    internal interface IProductService
     {
-        List<Product> GetProducts();
     }
 }
