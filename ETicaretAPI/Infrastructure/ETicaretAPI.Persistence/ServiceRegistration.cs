671dc254164c9e3f06e9b881b15fd6aee413947a
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-09 21:45:46 +0300

    3 datas added on databases

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -1,13 +1,20 @@
-﻿namespace ETicaretAPI.Persistence
+﻿using ETicaretAPI.Persistence.Repositories;
+
+namespace ETicaretAPI.Persistence
 {
     public static class ServiceRegistration
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddDbContext<ETicaretAPIDbContext>(options =>
-            {
-                options.UseNpgsql("User ID=postgres;Password=12345;Host=localhost;Port=5432;Database=ETicaretAPIDb;");
-            });
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString),ServiceLifetime.Singleton);
+
+
+            services.AddSingleton<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
+            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
+            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
         }
     }
 }
