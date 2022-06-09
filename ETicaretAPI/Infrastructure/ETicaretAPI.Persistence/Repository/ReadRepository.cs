0455b0275cdbe30c52d6fad25a3982d42268921d
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-09 22:01:28 +0300

    add  bool tracking = true

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repository/ReadRepository.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repository/ReadRepository.cs
@@ -11,14 +11,14 @@
 
         public DbSet<T> Table => _context.Set<T>();
 
-        public IQueryable<T> GetAll() => Table;
+        public IQueryable<T> GetAll(bool tracking = true) => Table;
 
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method) => Table.Where(method);
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true) => Table.Where(method);
 
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
             => await Table.FirstOrDefaultAsync(method);
 
-        public async Task<T> GetByIdAsync(string id)
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
             => await Table.FirstOrDefaultAsync(p => p.Id == Guid.Parse(id));
     }
 }
