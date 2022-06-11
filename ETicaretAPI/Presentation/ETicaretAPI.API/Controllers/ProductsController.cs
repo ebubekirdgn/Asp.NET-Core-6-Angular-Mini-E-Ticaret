5b993cf118cb233b0d7711327637657d691e510c
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-11 09:02:26 +0300

    interceptor added

--- ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -7,19 +7,27 @@ namespace ETicaretAPI.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        readonly private  IProductWriteRepository _productWriteRepository;
-        readonly private IProductReadRepository _productReadRepository;
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+        private readonly IOrderWriteRepository _orderWriteRepository;
+        private readonly ICustomerWriteRepository _customerWriteRepository;
 
-        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository, IOrderWriteRepository orderWriteRepository, ICustomerWriteRepository customerWriteRepository)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _customerWriteRepository = customerWriteRepository;
         }
 
         [HttpGet]
-        public async void Get()
+        public async Task Get()
         {
-           
+            var customerId = Guid.NewGuid();
+            await _customerWriteRepository.AddAsync(new() {Name="Ebubekir",Id= customerId });
+            await  _orderWriteRepository.AddAsync(new() { Description ="asdasd",Address="Ankara",CustomerId= customerId});
+            await _orderWriteRepository.AddAsync(new() { Description ="1",Address="Ankara,DEneme", CustomerId = customerId });
+            await _orderWriteRepository.SaveAsync();
         }
     }
 }
