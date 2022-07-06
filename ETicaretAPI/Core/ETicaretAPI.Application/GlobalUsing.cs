aa52eccdfc7268daa404dc440fe7e9e4f264acb0
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-07-06 23:19:46 +0300

    cleaned code

--- ETicaretAPI/Core/ETicaretAPI.Application/GlobalUsing.cs
+++ ETicaretAPI/Core/ETicaretAPI.Application/GlobalUsing.cs
@@ -1,7 +1,4 @@
-﻿global using ETicaretAPI.Domain.Entities.Common;
+﻿global using ETicaretAPI.Domain.Entities;
+global using ETicaretAPI.Domain.Entities.Common;
 global using Microsoft.EntityFrameworkCore;
 global using System.Linq.Expressions;
-global using ETicaretAPI.Domain.Entities;
-global using ETicaretAPI.Domain.Entities;
-
-
