64b274763f405c78bd8565c2caec9116a83a80be
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-07-06 23:20:20 +0300

    updated

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/GlobalUsing.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/GlobalUsing.cs
@@ -2,6 +2,7 @@
 global using ETicaretAPI.Domain.Entities;
 global using ETicaretAPI.Domain.Entities.Common;
 global using ETicaretAPI.Persistence.Context;
+global using ETicaretAPI.Persistence.Repositories;
 global using ETicaretAPI.Persistence.Repository;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.ChangeTracking;
@@ -9,5 +10,3 @@ global using Microsoft.EntityFrameworkCore.Design;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using System.Linq.Expressions;
-global using ETicaretAPI.Persistence.Repositories;
-
