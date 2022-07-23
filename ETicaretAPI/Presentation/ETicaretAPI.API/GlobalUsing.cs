7c4990642942e265f731642317576bb06c19c3db
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-07-23 23:25:00 +0300

    api.apiyede fluentvalidation kuruldu yoksa hata veriyordu

--- ETicaretAPI/Presentation/ETicaretAPI.API/GlobalUsing.cs
+++ ETicaretAPI/Presentation/ETicaretAPI.API/GlobalUsing.cs
@@ -3,3 +3,6 @@ global using ETicaretAPI.Application.ViewModel.Products;
 global using ETicaretAPI.Persistence;
 global using Microsoft.AspNetCore.Mvc;
 global using System.Net;
+global using ETicaretAPI.Application.Validators.Products;
+global using ETicaretAPI.Infrastructure.Filters;
+global using Microsoft.Extensions.DependencyInjection;
--- ETicaretAPI/Presentation/ETicaretAPI.API/Program.cs
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Program.cs
@@ -1,12 +1,18 @@
+using FluentValidation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddPersistenceServices();
-builder.Services.AddControllers();
 
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
     policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()
 ));
+
+builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+                .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+                .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
