d7f0c9ebac0d211ff3eef2ffb3ce39aa709bc28d
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-07-23 21:34:45 +0300

    cleaned code

--- ETicaretAPI/Core/ETicaretAPI.Application/GlobalUsing.cs
+++ ETicaretAPI/Core/ETicaretAPI.Application/GlobalUsing.cs
@@ -1,6 +1,6 @@
-﻿global using ETicaretAPI.Domain.Entities;
+﻿global using ETicaretAPI.Application.ViewModel.Products;
+global using ETicaretAPI.Domain.Entities;
 global using ETicaretAPI.Domain.Entities.Common;
+global using FluentValidation;
 global using Microsoft.EntityFrameworkCore;
 global using System.Linq.Expressions;
-global using ETicaretAPI.Application.ViewModel.Products;
-global using FluentValidation;
